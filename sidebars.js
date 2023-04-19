ef4f3a060cb978428b8d200f46880366c1d4b125
Grégory Libert <gl@massa.net>
2023-04-19 08:40:16 +0200

    Fix sidebar

--- sidebars.js
+++ sidebars.js
@@ -26,7 +26,7 @@ const sidebars = {
         },
         {
           type: "category",
-          label: "Code",
+          label: "Develop",
           items: [
             {
               type: "doc",
