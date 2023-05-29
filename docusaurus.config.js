94a02921d4d7e1e0df647fde3f7620712646e9b5
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-05-29 15:01:09 +0200

    add build section (#41)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,61 +1,60 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Massa Documentation',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/logo.svg',
+  title: "Massa Documentation",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/logo.svg",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: "https://your-docusaurus-test-site.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docu-dev/',
+  baseUrl: "/docu-dev/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'massalabs', // Usually your GitHub org/user name.
-  projectName: 'docu-dev', // Usually your repo name.
+  organizationName: "massalabs", // Usually your GitHub org/user name.
+  projectName: "docu-dev", // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/massalabs/docu-dev/tree/main/',
+          editUrl: "https://github.com/massalabs/docu-dev/tree/main/",
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           remarkPlugins: [math],
@@ -63,86 +62,92 @@ const config = {
         },
         blog: {},
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
-      }),
+      },
     ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
+    {
       // Replace with your project's social card
       navbar: {
-        title: 'Docs',
+        title: "Docs",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/massa_logo.svg',
-          srcDark: 'img/massa_logo_white.svg',
+          alt: "My Site Logo",
+          src: "img/massa_logo.svg",
+          srcDark: "img/massa_logo_white.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'learnSidebar',
-            position: 'left',
-            label: 'Learn',
+            type: "docSidebar",
+            sidebarId: "learnSidebar",
+            position: "left",
+            label: "Learn",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'nodeSidebar',
-            position: 'left',
-            label: 'Testnet',
+            type: "docSidebar",
+            sidebarId: "buildSidebar",
+            position: "left",
+            label: "🛠️ Build",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'rpcSidebar',
-            position: 'left',
-            label: 'RPC',
+            type: "docSidebar",
+            sidebarId: "nodeSidebar",
+            position: "left",
+            label: "Testnet",
           },
           {
-            href: 'https://github.com/massalabs/',
-            label: 'GitHub',
-            position: 'right',
+            type: "docSidebar",
+            sidebarId: "rpcSidebar",
+            position: "left",
+            label: "RPC",
+          },
+          {
+            href: "https://github.com/massalabs/",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/smart-contract/intro',
+                label: "Tutorial",
+                to: "/docs/smart-contract/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/massa',
+                label: "Discord",
+                href: "https://discord.gg/massa",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/massalabs',
+                label: "Twitter",
+                href: "https://twitter.com/massalabs",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/massalabs',
+                label: "GitHub",
+                href: "https://github.com/massalabs",
               },
             ],
           },
@@ -153,7 +158,7 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
-    }),
+    },
 };
 
 module.exports = config;
