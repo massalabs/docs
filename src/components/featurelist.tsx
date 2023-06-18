fb97546df8b8374dfa1c9503b272c3e752d75d06
Adrien LF <l.neirda@gmail.com>
2023-06-18 11:20:25 +0200

    Revert "Add massa-sc-examples"
    This reverts commit d6d590691bfcb41b656c7bba5a38d0735a55ea82.

--- src/components/featurelist.tsx
+++ src/components/featurelist.tsx
@@ -12,15 +12,11 @@ export function FeatureList({ children }) {
   </>
 }
 
-export function Feature({ url, title, content, icon }) {
-  let link;
-  if (url.startsWith('http')) {
-    link = <svg width="0.8rem" height="0.8rem" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>;
-  }
+export function Feature({ docName, title, content, icon }) {
   return <>
-    <a className={clsx("col col--4", styles.feature)} href={url}>
+    <a className={clsx("col col--4", styles.feature)} href={docName}>
       <div>
-        <div className="avatar__name">{icon} {title} {link}</div>
+        <div className="avatar__name">{icon} {title}</div>
         <small>{content}</small>
       </div>
     </a>
