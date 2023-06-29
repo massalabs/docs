aa3a9c118ed4dcb82581a89fa203a5ca446e8f13
Adrien Laversanne-Finot <alf@massa.net>
2023-06-29 16:00:46 +0200

    Change homepage title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Welcome to Massa`}
       description="Description will go into a meta tag in <head />">
       <main>
         <HomepageFeatures />
