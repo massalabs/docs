bf2d87eabf1f6686fa2286fbe0e26eeeedd88ace
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-07-13 16:06:17 +0200

    trgger ci + first draft of code extractor

---
+++ docs/build/code-fetched/extract-test-code.ts
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+function extractTestCode(fileName, testName) {
+  const data = fs.readFileSync(path.resolve(__dirname, fileName), 'utf-8');
+  const lines = data.split('\n');
+  let startLine = -1;
+  let endLine = -1;
+
+  lines.forEach((line, index) => {
+    if (line.includes(`/* Test function ${testName}`)) {
+      startLine = index;
+    }
+    if (line.includes(`/* End of test */`) && startLine !== -1 && endLine === -1) {
+      endLine = index;
+    }
+  });
+
+  if (startLine !== -1 && endLine !== -1) {
+    return lines.slice(startLine + 2, endLine).join('\n');
+  } else {
+    return '';
+  }
+}
+
+const testCode = extractTestCode('test-wallet.ts', 'walletGenerateNewAccount');
+console.log(testCode);
