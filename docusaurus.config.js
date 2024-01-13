64bdf04aaa12497f9ab7d7777e01ae9ac8329f99
Adrien <alf@massa.net>
2024-01-13 23:04:20 +0100

    fix link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/massalabs/docu-dev/tree/main/",
+          editUrl: "https://github.com/massalabs/docs/tree/main/",
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           remarkPlugins: [math, remarkGridTables],
