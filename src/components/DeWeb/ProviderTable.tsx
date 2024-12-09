3b779672e1c88a1276de53dcfb4d708c256612bf
thomas-senechal <thomas.senechal@pm.me>
2024-12-09 10:52:46 +0100

    Add ProviderTable and WebsiteTable components to display providers and websites

---
+++ src/components/DeWeb/ProviderTable.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import { parse } from "yaml";
+
+const YAML_URL =
+  "https://raw.githubusercontent.com/massalabs/DeWeb-Providers/refs/heads/main/providers.yaml";
+
+interface Provider {
+  title: string;
+  desc: string;
+  url: string;
+  owner: string;
+}
+
+const ProviderTable: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(YAML_URL, {
+          redirect: "follow",
+        });
+        if (!response.ok) {
+          setError(`HTTP error! Status: ${response.status}`);
+          return;
+        }
+        const text = await response.text();
+        const data = parse(text) as Provider[];
+        setProviders(data);
+      } catch (err) {
+        setError((err as Error).message);
+        console.error("Error loading YAML file:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>URL</th>
+            <th>Description</th>
+            <th>Owner</th>
+          </tr>
+        </thead>
+        <tbody>
+          {error && <tr>{error}</tr>}
+          {providers.map((site, index) => (
+            <tr key={index}>
+              <td>{site.title}</td>
+              <td>
+                <a href={site.url} target="_blank" rel="noopener noreferrer">
+                  {site.url}
+                </a>
+              </td>
+              <td>{site.desc}</td>
+              <td>{site.owner}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProviderTable;
